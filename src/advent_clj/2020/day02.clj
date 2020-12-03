b2b322d8bd01fb2a480f5c17c3c5f76df26a5a75
Andrew J. Pierce <andrew.j.pierce@gmail.com>
2020-12-03 12:16:52 -0500

    Add solution for 2020-02

---
+++ src/advent_clj/2020/day02.clj
@@ -0,0 +1,34 @@
+(ns advent-clj.2020.day02
+  (:require [advent-clj.core :refer [get-puzzle-input]])
+  (:gen-class))
+
+(def input (get-puzzle-input "2020/day02.txt"))
+
+(defn format-entry [x]
+  (let [[rule letter pass] (clojure.string/split x #" ")]
+    (list (map #(Integer/parseInt %) (clojure.string/split rule #"-"))
+          (first letter)
+          pass)))
+
+(defn valid-pass?
+  "Checks to see if the formatted entry is valid accoring to part 1"
+  [[[mn mx] letter pass]]
+  (let [occurrences (get (frequencies pass) letter)]
+    (cond (nil? occurrences) false
+          (and (>= occurrences mn) (<= occurrences mx)) true
+          :else false)))
+
+(defn valid-pass??
+  "Checks to see if the formatted entry is valid accoring to part 2"
+  [[[mn mx] letter pass]]
+  (let [one (get pass (dec mn))
+        two (get pass (dec mx))]
+    (and (not-every? #(= letter %) [one two])
+         (some #(= letter %) [one two]))))
+
+(def part1 (->> input (map format-entry) (filter valid-pass?) count))
+(def part2 (->> input (map format-entry) (filter valid-pass??) count))
+
+(defn -main []
+  (println "Advent of Code 2020-02.1:" part1)
+  (println "Advent of Code 2020-02.2:" part2))

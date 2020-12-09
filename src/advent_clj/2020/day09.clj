b7d8a0cbdec051834ece73ef4c8e4515f0d03b1e
Andrew J. Pierce <andrew.j.pierce@gmail.com>
2020-12-09 07:54:36 -0500

    Tweak 2020.09 to use loop/recur instead of list comprehensions

--- src/advent_clj/2020/day09.clj
+++ src/advent_clj/2020/day09.clj
@@ -19,14 +19,26 @@
        first last))
 
 (def part2
-  (->> (for [subset-size (range 2 21)  ;; 20 is an arbitrary max subset size to keep it fast
-             :let [consec-nums (partition subset-size 1 input)
-                   matching (filter #(= part1 (reduce + %)) consec-nums)]
-             :when (not (empty? matching))]
-         matching)
-       flatten
-       (#(+ (apply min %) (apply max %)))))
+  (loop [subset-size 2]
+    (if (> subset-size (count input)) nil
+        (let [consec-nums (partition subset-size 1 input)
+              matching (filter #(= part1 (reduce + %)) consec-nums)]
+          (if (empty? matching) (recur (inc subset-size))
+              (+ (apply min (flatten matching)) (apply max (flatten matching))))))))
 
 (defn -main []
   (println "Advent of Code 2020-09.1:" part1)
   (println "Advent of Code 2020-09.2:" part2))
+
+
+(comment
+  "Here was my original solution to part 2. It was slower"
+  (def part2
+    (->> (for [subset-size (range 2 21)  ;; 20 is an arbitrary max subset size to keep it fast
+               :let [consec-nums (partition subset-size 1 input)
+                     matching (filter #(= part1 (reduce + %)) consec-nums)]
+               :when (not (empty? matching))]
+           matching)
+         flatten
+         (#(+ (apply min %) (apply max %)))))
+)

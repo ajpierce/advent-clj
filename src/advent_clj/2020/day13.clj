354031bf13c391aadb8193854a3f169d38d6edde
Andrew J. Pierce <andrew.j.pierce@gmail.com>
2020-12-13 16:31:47 -0500

    Solve 2020.13 part2 brute-force

--- src/advent_clj/2020/day13.clj
+++ src/advent_clj/2020/day13.clj
@@ -28,6 +28,32 @@
          (#(let [[bdt bid] %]
              (* bid (- bdt dt)))))))
 
+(defn parse-pt2 [in]
+  (->> (s/split (last in) #",")
+       (map-indexed (fn [idx busid]
+                      (try [idx (Integer/parseInt busid)]
+                           (catch Exception _ nil))))
+       (remove nil?)))
+
+(defn valid? [offset busid i]
+  (zero? (mod (+ i offset) busid)))
+
+(defn satisfies-all? [validators n]
+  (every? true? (map #(% n) validators)))
+
+(defn fastest-bus-reducer [[_ fbus :as acc] [offset bus]]
+  (if (< fbus bus) [offset bus] acc))
+
+#_(def part2
+    (time
+     (let [busses (parse-pt2 samp)
+           validators (map (fn [[offset busid]] (partial valid? offset busid)) busses)
+           good? (partial satisfies-all? validators)
+           [fo fb] (reduce fastest-bus-reducer busses)]
+       (->> (iterate (partial + fb) (- fb fo))
+            (filter good?)
+            (first)))))
+
 (defn -main []
   (println "Advent of Code 2020.13.1:" part1)
   #_(println "Advent of Code 2020.13.2:" part2))

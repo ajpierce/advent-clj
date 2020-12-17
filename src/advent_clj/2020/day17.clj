1ef40ca3e112ff6aa535ce9a1afbe8240470e706
Andrew J. Pierce <andrew.j.pierce@gmail.com>
2020-12-17 11:49:44 -0500

    Solve 2020.17 part 2

--- src/advent_clj/2020/day17.clj
+++ src/advent_clj/2020/day17.clj
@@ -1,11 +1,16 @@
 (ns advent-clj.2020.day17
   (:require [advent-clj.core :refer [get-puzzle-input]]))
 
-(defn parse [^Integer y ^String row]
-  (->> row seq
-       (map-indexed
-        (fn [x v] (let [coords [0 y x]]
-                    [coords (if (= \# v) :on :off)])))))
+(def input (get-puzzle-input "2020/day17.txt"))
+(def samp '(".#." "..#" "###"))
+
+(defn parse
+  ([^Integer y ^String row] (parse false y row))
+  ([hyper? y row]
+   (->> row seq
+        (map-indexed
+         (fn [x v] (let [coords (if hyper? [0 0 y x] [0 y x])]
+                     [coords (if (= \# v) :on :off)]))))))
 
 (defn init-universe [raw-input]
   (->> raw-input
@@ -14,11 +19,8 @@
        (reduce (fn [acc [z y x v]]
                  (let [k [z y x]] (assoc acc k v))) (sorted-map))))
 
-(def input (init-universe (get-puzzle-input "2020/day17.txt")))
-(def samp (init-universe '(".#." "..#" "###")))
-
 (defn adjacent [n] (range (dec n) (+ 2 n)))
-(defn neighbors [[z y x]]
+(defn regular-neighbors [[z y x]]
   (for [x' (adjacent x)
         y' (adjacent y)
         z' (adjacent z)
@@ -26,29 +28,53 @@
     [z' y' x']))
 
 (defn active? [v] (= :on v))
-(defn active-neighbors [universe coords]
+(defn active-neighbors [neighbors universe coords]
   (->> coords
        neighbors
        (map #(get universe %))
        (filter active?)
        count))
 
-(defn step [universe]
+(defn step [neighbors universe]
   (let [cubes-to-change (reduce #(apply conj %1 %2) (sorted-set) (map neighbors (keys universe)))]
     (loop [uu (sorted-map) remaining cubes-to-change]
       (if (empty? remaining) uu
           (let [current-cube (first remaining)
                 current-state (get universe current-cube)
-                nearby-active (active-neighbors universe current-cube)
+                nearby-active (active-neighbors neighbors universe current-cube)
                 new-state (cond (and (active? current-state)
                                      (or (= 2 nearby-active) (= 3 nearby-active))) :on
                                 (and (not (active? current-state)) (= 3 nearby-active)) :on
                                 :else :off)]
-            #_(println current-cube "is" current-state ", has" nearby-active "neighbors. Becomes:" new-state)
             (recur (assoc uu current-cube new-state) (rest remaining)))))))
 
 (def part1
-  (->> (nth (iterate step input) 6)
+  (->> (nth (iterate (partial step regular-neighbors) (init-universe input)) 6)
        (vals)
        (filter active?)
        (count)))
+
+(defn hyper-neighbors [[h z y x]]
+  (for [x' (adjacent x)
+        y' (adjacent y)
+        z' (adjacent z)
+        h' (adjacent h)
+        :when (not= [h z y x] [h' z' y' x'])]
+    [h' z' y' x']))
+
+(defn init-4d-universe [raw-input]
+  (->> raw-input
+       (map-indexed (partial parse true))
+       flatten (partition 5)
+       (reduce (fn [acc [h z y x v]]
+                 (let [k [h z y x]] (assoc acc k v))) (sorted-map))))
+
+(def part2
+  (->> (nth (iterate (partial step hyper-neighbors) (init-4d-universe input)) 6)
+       (vals)
+       (filter active?)
+       (count)))
+
+(defn -main []
+  (println "Advent of Code 2020.17.1:" part1)
+  (println "Advent of Code 2020.17.2:" part2))

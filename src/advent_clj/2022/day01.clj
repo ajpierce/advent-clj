832d364fc66ba07461b801f1477e1d4582357d23
Andrew J. Pierce <andrew.j.pierce@gmail.com>
2022-12-01 20:39:11 -0500

    Frontload computation and simplify part1/2 (2022.01)

--- src/advent_clj/2022/day01.clj
+++ src/advent_clj/2022/day01.clj
@@ -2,16 +2,18 @@
   (:require [advent-clj.core :refer [get-puzzle-input]])
   (:gen-class))
 
-(def input (->> (get-puzzle-input "2022/day01.txt")
-                (partition-by empty?)
-                (remove (comp empty? first))
-                (map #(map read-string %))))
+(def sum-as-ints (comp (partial reduce +)
+                       (partial map read-string)))
 
-(def part1 (time (->> input (map #(reduce + %)) (apply max))))
-(def part2 (time (->> input (map #(reduce + %)) sort reverse (take 3) (reduce +))))
+(def sorted-sums (->> (get-puzzle-input "2022/day01.txt")
+                      (partition-by empty?)
+                      (remove (comp empty? first))
+                      (map sum-as-ints)
+                      sort reverse))
+
+(def part1 (first sorted-sums))
+(def part2 (reduce + (take 3 sorted-sums)))
 
 (defn -main []
   (println "Advent of Code 2022-01.1:" part1)
   (println "Advent of Code 2022-01.2:" part2))
-
-(comment "")
